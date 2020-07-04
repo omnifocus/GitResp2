195a902ce4b0987652b67f1fd874b9b9feb97d15
omnifocus <omnifocus@protonmail.com>
2020-07-04 23:17:54 +0800

    在Person中增加了age属性

---
+++ TestModule01/src/com/yovya/Person.java
@@ -0,0 +1,11 @@
+package com.yovya;
+
+/**
+ * @author: yovya
+ * @Date: 2020/7/4 - 07 - 04 - 11:16 PM
+ * @Description: com.yovya
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
